1a82419a90bf4223014ba32fe547ee7068e0a621
longh <longh@DESKTOP-IBJT1UF>
2021-06-24 20:28:03 +0700

    Add project files.

---
+++ VnExpress_DinhHaiLong/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VnExpress_DinhHaiLong.MvcApplication" Language="C#" %>
---
+++ VnExpress_DinhHaiLong/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VnExpress_DinhHaiLong.Startup))]
+namespace VnExpress_DinhHaiLong
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
